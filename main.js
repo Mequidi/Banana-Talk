65355320350f60cb27675d02c27248de15315c60
Mequidi <nikhilsejwal234@gmail.com>
2021-05-15 23:44:59 +0530

    input output in js

---
+++ main.js
@@ -0,0 +1,15 @@
+var btntranslate = document.querySelector("#translate");
+btntranslate.addEventListener("click",clickEventHandler);
+
+var translateinput = document.querySelector("#txtinput");
+
+var translateoutput = document.querySelector("#translate-box");
+
+
+function clickEventHandler()
+{
+    console.log("button is clicked !");
+    
+
+}
+
