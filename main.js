4335535fdab514e5977dea9d34872bca41935af7
Mequidi <nikhilsejwal234@gmail.com>
2021-05-18 15:44:06 +0530

    completed js

--- main.js
+++ main.js
@@ -5,11 +5,27 @@ var translateinput = document.querySelector("#txtinput");
 
 var translateoutput = document.querySelector("#translate-box");
 
+var url = "https://api.funtranslations.com/translate/minion.json"
 
 function clickEventHandler()
 {
     console.log("button is clicked !");
-    
-
+    var input = translateinput.nodeValue;
+    var finalURL = constuctURL(input);
+    console.log(finalURL);
+    fetch(finalURL)
+        .then(response=>response.json)
+        .then(json=>{
+                translateoutput.innerText = json.contents.translated;
+        })
+        .catch(()=>alert("something went wrong!"))
 }
+
+function constuctURL(i)
+{
+    var encodeURL = url + "?text=" + i ;
+    return encodeURL;
+}
+
+
 
